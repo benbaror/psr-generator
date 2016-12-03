37e7c465052ac53250b2a8a46a3f001ba35398dd
Ben Bar-Or <benbaror@gmail.com>
2016-12-03 02:42:23 -0500

    Use position of the sun

--- tests/test_psr_generator.py
+++ tests/test_psr_generator.py
@@ -18,24 +18,31 @@ def test_main():
 
 
 def test_pulsar():
-    tol = 1e-3
-    pulsar = Pulsar(1e-3, 0, 8.3, unit=['deg', 'deg', 'kpc'],
+    tol = 1e-4
+    pulsar = Pulsar(0, 0, 0, unit=['deg', 'deg', 'kpc'],
                     frame='galactic')
-    assert all(abs(pulsar.galactocentric.cartesian.xyz.value) < tol)  # pi/3600/180 * (pc/km) * (s/yr)
+    glc = pulsar.galactocentric
+    dx = abs(1 - glc.cartesian.x/(- glc.galcen_distance)).value
+    dy = abs(glc.cartesian.y).value
+    dz = (1 - glc.cartesian.z/glc.z_sun).value
+    assert all(np.array([dx, dy, dz]) < tol)
 
 def test_random_pulsar():
-    tol = 1e-3
+    tol = 1e-4
 
     pulsars = RandomPulsar(R0=4, beta=1, z0=0.2, size=10)
     assert len(pulsars) == 10
 
-    x, y, z = pulsars.galactocentric.cartesian.xyz.value
-    r = np.sqrt((x + 8.3)**2 + y**2 + z**2)
-    assert all(abs(1 - pulsars.galactic.distance.value/r) < tol)
+    x, y, z = pulsars.galactocentric.cartesian.xyz
+    r = np.sqrt((x + pulsars.galcen_distance)**2 +
+                y**2 +
+                (z - pulsars.z_sun)**2)
+    assert all(abs(1 - pulsars.galactic.distance/r) < tol)
 
     rpulsars = RandomPulsar(x=x, y=y, z=z, unit='kpc', frame='galactocentric')
     pulsars = Pulsar(x=x, y=y, z=z, unit='kpc', frame='galactocentric')
-    assert all(abs(1 - rpulsars.galactic.distance.value/rpulsars.galactic.distance.value) < tol)
+    assert all(abs(1 - rpulsars.galactic.distance/rpulsars.galactic.distance) <
+               tol)
 
 def test_galactic_dist():
     galactic_dist = GalacticDistribution(R0=10, beta=2, z0=0.1)
